ead72010e809a5beef3b75d634f758d20ff17aa6
Marty Mathis <marty.mathis@gmail.com>
2018-04-01 20:56:05 -0400

    Simplified file names.

--- src/PipelineFramework/Abstractions/IPipelineComponent.cs
+++ src/PipelineFramework/Abstractions/IPipelineComponent.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
 
 namespace PipelineFramework.Abstractions
 {
@@ -18,5 +19,20 @@ namespace PipelineFramework.Abstractions
         /// <param name="name">Name of the component.</param>
         /// <param name="settings">Configuration settings.</param>
         void Initialize(string name, IDictionary<string, string> settings);
+    }
+
+    /// <summary>
+    /// Defines the behavior of a pipeline component.
+    /// </summary>
+    /// <typeparam name="T">The object that participates in the execution of the pipeline component.</typeparam>
+    public interface IPipelineComponent<T> : IPipelineComponent
+    {
+        /// <summary>
+        /// Executes this pipeline component.
+        /// </summary>
+        /// <param name="payload">Payload object this instance uses for execution.</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Payload object returned after this instance execution complete.</returns>
+        T Execute(T payload, CancellationToken cancellationToken);
     }
 }
