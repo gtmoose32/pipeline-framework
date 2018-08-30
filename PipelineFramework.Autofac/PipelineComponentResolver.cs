22b1faa03231844168c9123cf8cc9cdf6a059cc4
Marty Mathis <marty.mathis@gmail.com>
2018-08-30 13:09:42 -0400

    Autofac project addition.

---
+++ PipelineFramework.Autofac/PipelineComponentResolver.cs
@@ -0,0 +1,13 @@
+﻿using PipelineFramework.Abstractions;
+using System;
+
+namespace PipelineFramework.Autofac
+{
+    public class PipelineComponentResolver : IPipelineComponentResolver
+    {
+        public T GetInstance<T>(string name) where T : IPipelineComponent
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
