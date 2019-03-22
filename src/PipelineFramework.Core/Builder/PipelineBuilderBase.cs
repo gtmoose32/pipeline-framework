ac367761c0475b35252ca16df94b12678581ada4
Andrew Moreno <Andrew.Moreno@assurant.com>
2019-03-22 15:54:54 -0400

    Extract PipelineBuilderBase from Async/NonAsync PipelineBuilders

---
+++ src/PipelineFramework.Core/Builder/PipelineBuilderBase.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using PipelineFramework.Abstractions;
+using PipelineFramework.Builder.Interfaces;
+
+namespace PipelineFramework.Builder
+{
+    internal abstract class PipelineBuilderBase<TPipeline, TComponentBase, TPayload> : 
+        IPipelineBuilder<TPipeline>,
+        ISettingsHolder<TPipeline>, 
+        IAdditionalPipelineComponentHolder<TPipeline, TComponentBase, TPayload> where TPipeline : IPipeline
+        where TComponentBase : IPipelineComponent
+    {
+        protected readonly PipelineBuilderState State;
+
+        protected PipelineBuilderBase()
+        {
+            State = new PipelineBuilderState();
+        }
+
+        public abstract TPipeline Build();
+
+        public IPipelineBuilder<TPipeline> WithSettings(IDictionary<string, IDictionary<string, string>> settings)
+        {
+            State.Settings = settings;
+            return this;
+        }
+
+        public IPipelineBuilder<TPipeline> WithNoSettings()
+        {
+            State.UseDefaultSettings();
+            return this;
+        }
+
+        public ISettingsHolder<TPipeline> WithComponentResolver(IPipelineComponentResolver componentResolver)
+        {
+            State.ComponentResolver = componentResolver;
+            return this;
+        }
+
+        public IAdditionalPipelineComponentHolder<TPipeline, TComponentBase, TPayload> WithComponent<TComponent>() where TComponent : TComponentBase
+        {
+            State.AddComponent(typeof(TComponent));
+            return this;
+        }
+
+        public IAdditionalPipelineComponentHolder<TPipeline, TComponentBase, TPayload> WithComponent(string componentName)
+        {
+            State.AddComponent(componentName);
+            return this;
+        }
+    }
+}
